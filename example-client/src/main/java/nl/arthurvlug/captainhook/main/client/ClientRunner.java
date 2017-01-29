8e817b82c526989308991db467c57f905034228e
Arthur <akmo75@gmail.com>
2017-01-29 17:08:43 +0000

    Fixed build scripts so they don't run the applications; Also sleep after an exception on the server side; Sleep in the client if the service is not ready yet

--- example-client/src/main/java/nl/arthurvlug/captainhook/main/client/ClientRunner.java
+++ example-client/src/main/java/nl/arthurvlug/captainhook/main/client/ClientRunner.java
@@ -2,11 +2,12 @@ package nl.arthurvlug.captainhook.main.client;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import nl.arthurvlug.captainhook.exampleservice.activity.helloworld.HelloWorldInput;
+import nl.arthurvlug.captainhook.exampleservice.activity.helloworld.HelloWorldOutput;
 import nl.arthurvlug.captainhook.exampleservice.client.ExampleServiceClient;
 import nl.arthurvlug.captainhook.framework.client.AbstractClientRunner;
 import nl.arthurvlug.captainhook.framework.common.response.DependencyException;
-import nl.arthurvlug.captainhook.exampleservice.activity.helloworld.HelloWorldInput;
-import nl.arthurvlug.captainhook.exampleservice.activity.helloworld.HelloWorldOutput;
+import org.apache.http.conn.HttpHostConnectException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +38,16 @@ public class ClientRunner extends AbstractClientRunner {
             final HelloWorldOutput output = exampleService.helloWorldCall(helloWorldInput).call();
             logger.info(output.getMessage());
         } catch (DependencyException e) {
-            logger.error(e.getCause().getClass().getSimpleName() + ": " + e.getCause().getMessage());
+            if(e.getCause() instanceof HttpHostConnectException) {
+                logger.error("Server not ready. Waiting 500 ms");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e2) {
+                    throw new RuntimeException(e2);
+                }
+            } else {
+                logger.error(e.getCause().getClass().getSimpleName() + ": " + e.getCause().getMessage());
+            }
         }
     }
 }
--- example-service/src/main/java/nl/arthurvlug/captainhook/exampleservice/activity/helloworld/HelloWorldActivity.java
+++ example-service/src/main/java/nl/arthurvlug/captainhook/exampleservice/activity/helloworld/HelloWorldActivity.java
@@ -7,10 +7,10 @@ import nl.arthurvlug.captainhook.framework.server.Activity;
 public class HelloWorldActivity extends AbstractExampleActivity<HelloWorldInput, HelloWorldOutput> {
     @Override
     public HelloWorldOutput enact(HelloWorldInput helloWorldInput) throws InterruptedException {
+        Thread.sleep((long) (Math.random() * 2000));
         if(Math.random() < 0.3) {
-            throw new RuntimeException("Wrong argument");
+            throw new RuntimeException("Purposely thrown an exception");
         }
-        Thread.sleep((long) (Math.random() * 1000));
         return new HelloWorldOutput("Hello, " + helloWorldInput.getName() + "!");
     }
 }
