0be89636ed09b693df0480224e96869d24bd3c56
Arthur <akmo75@gmail.com>
2017-03-04 21:47:52 +0000

    Spring template component only in ServerSpringComponentsImporter

--- framework-core-server-spring/src/main/template/com/arthurvlug/_service/server/ServerSpringComponentsImporter.java
+++ framework-core-server-spring/src/main/template/com/arthurvlug/_service/server/ServerSpringComponentsImporter.java
@@ -1,15 +1,15 @@
 package com.arthurvlug._service.server;
 
+import com.arthurvlug._service.server.ServiceConfiguration;
 import lombok.Getter;
-import nl.arthurvlug.captainhook.framework.common.AbstractClientWrapperProvider;
 import nl.arthurvlug.captainhook.framework.server.AbstractServerComponentsImporter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @Import({
-        ServiceConfiguration.class,
         AbstractClientWrapperProvider.class
 })
 public class ServerSpringComponentsImporter implements AbstractServerComponentsImporter {
-    @Getter
+    @Getter(onMethod = @__(@Bean))
     private final ServiceConfiguration serviceConfiguration = new ServiceConfiguration();
 }
